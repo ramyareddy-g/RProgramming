5a90abc8f18a1d9b18d1af468364831dc0c0e8ed
ramyareddy-g <ramyagangula@gmail.com>
2018-09-13 22:02:48 +0530

    nested functions

---
+++ nested_functions_log_calculatation.R
@@ -0,0 +1,10 @@
+# log to the base 2 
+log2(16)
+
+# sqrt of the log to the base 2 of 16:
+sqrt(log2(16))
+
+# Compute log to the base 10 (log10) of the sqrt of 100. Do not use variables.
+log(sqrt(100),10)
+# or 
+log10(sqrt(100))
