30b54fb3b5be93b000e437a7d58fd8e92d5b808e
ramyareddy-g <ramyagangula@gmail.com>
2018-11-03 00:35:47 +0530

    exploring more in ggplot
    adding aesthetics to the plot and removing plots related to NA’s in
    gender variable

--- ggplot.R
+++ ggplot.R
@@ -2,12 +2,20 @@ library(ggplot2)
 getwd()
 pf<- read.csv("pseudo_facebook.tsv", sep = "\t")
 names(pf)
-ggplot(aes(x= friend_count), data = pf)+
+
+#ggplot histogram frien count
+ggplot(aes(x= friend_count), data =pf)+
+  geom_histogram(binwidth= 25)+
+  scale_x_continuous(limits = c(0,1000), breaks = seq(0,1000,50))+
+  facet_grid(~gender)
+
+
+#omitting NA's graph
+ggplot(aes(x= friend_count, fill= gender), data = subset(pf, !is.na(gender)))+
   geom_histogram(binwidth= 25)+
     scale_x_continuous(limits = c(0,1000), breaks = seq(0,1000,50))+
-      facet_grid(gender~.)
+      facet_grid(~gender)
 #qplot
 qplot(x= friend_count, data= pf, binwidth= 25)+
-  scale_x_continuous(limits = c(0,1000), breaks = seq(0,1000,50))+
-  #facet_wrap(~gender, ncol=2)
-  facet_grid(friend_count~gender)
+  scale_x_continuous(limits  = c(0,1000), breaks = seq(0,1000,50))
+  
