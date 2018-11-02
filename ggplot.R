c69c6f1ced9c3d1f3cf3ec2fb4540fc33ba24000
ramyareddy-g <ramyagangula@gmail.com>
2018-11-02 20:31:36 +0530

    included limits, bin width

--- ggplot.R
+++ ggplot.R
@@ -3,8 +3,11 @@ getwd()
 pf<- read.csv("pseudo_facebook.tsv", sep = "\t")
 names(pf)
 ggplot(aes(x= friend_count), data = pf)+
-  geom_histogram()+
-    scale_x_continuous(limits = c(0,1000))
+  geom_histogram(binwidth= 25)+
+    scale_x_continuous(limits = c(0,1000), breaks = seq(0,1000,50))+
+      facet_grid(gender~.)
 #qplot
-qplot(x= friend_count, data= pf)+
-  scale_x_continuous(limits = c(0,1000))
+qplot(x= friend_count, data= pf, binwidth= 25)+
+  scale_x_continuous(limits = c(0,1000), breaks = seq(0,1000,50))+
+  #facet_wrap(~gender, ncol=2)
+  facet_grid(friend_count~gender)
