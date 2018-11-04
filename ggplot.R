2bf9200c7c7efd3035e03f3ee3df12b9d214db0e
ramyareddy-g <ramyagangula@gmail.com>
2018-11-04 19:33:35 +0530

    exploring more

--- ggplot.R
+++ ggplot.R
@@ -1,6 +1,7 @@
+library(dplyr)
 library(ggplot2)
 getwd()
-pf<- read.csv("pseudo_facebook.tsv", sep = "\t")
+pf<- read.csv("/Users/santosh/Downloads/pseudo_facebook.tsv", sep = "\t")
 names(pf)
 
 #ggplot histogram frien count
@@ -15,6 +16,15 @@ ggplot(aes(x= friend_count, fill= gender), data = subset(pf, !is.na(gender)))+
   geom_histogram(binwidth= 25)+
     scale_x_continuous(limits = c(0,1000), breaks = seq(0,1000,50))+
       facet_grid(~gender)
+# determining which gender has the most friends count
+table(pf$gender)
+summary(pf$tenure)
+#tenure plot
+ggplot(aes(x= tenure/365, color= "black", fill= "green"), data= subset(pf, !is.na(gender)))+
+  geom_histogram(binwidth = 0.25)+
+  #scale_x_continuous(limits = c(0, 3600), breaks= c(0, 3600, 30))+
+  facet_wrap(~gender)
+
 #qplot
 qplot(x= friend_count, data= pf, binwidth= 25)+
   scale_x_continuous(limits  = c(0,1000), breaks = seq(0,1000,50))
